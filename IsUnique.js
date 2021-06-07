7274997d74cfdb1527d64f1599fa62d251199a96
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-06-06 23:27:50 -0400

    Bit Vector Approach

--- IsUnique.js
+++ IsUnique.js
@@ -4,6 +4,7 @@
 function isUnique(str){
     if(str.length > 128) return false;
     let obj = {}
+
     for(let i = 0; i < str.length; i++){
         if(obj[str[i]]){
             return false;
@@ -14,7 +15,26 @@ function isUnique(str){
     return true;
 }
 
-console.log(isUnique('catt'))
-console.log(isUnique('cat'))
-console.log(isUnique(''))
-console.log(isUnique('t'))
+// console.log(isUnique('catt'))
+// console.log(isUnique('cat'))
+// console.log(isUnique(''))
+// console.log(isUnique('t'))
+
+//bit vector 
+
+function isUniqueBitVector(str){
+    let checker = 0;
+    for(let i = 0; i < str.length; i++){
+        let charValue = str[i].charCodeAt(0) - 'a'.charCodeAt(0);
+        if((checker & (1 << charValue)) > 0){
+            return false;
+        }
+        checker |=  (1 << charValue);
+    }
+    return true;
+}
+
+console.log(isUniqueBitVector('catt'))
+console.log(isUniqueBitVector('cat'))
+console.log(isUniqueBitVector(''))
+console.log(isUniqueBitVector('t'))
