33ab012b944534a615e0a95eb679fcaad8e22432
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-06-27 15:10:58 -0400

    Vice versa for sorting matrix

--- SortTheMatrix.js
+++ SortTheMatrix.js
@@ -29,3 +29,33 @@ function diagonalSort(mat) {
 }
 
 console.log(diagonalSort([[3,3,1,1],[2,2,1,2],[1,1,1,2]]))
+
+
+//vice versa
+// let map = {};
+    
+//     for(let i=0; i < mat.length; i++){
+//         for(let j=0; j < mat[0].length; j++){
+            
+//             let d = i-j;
+          
+//             if(!map[d]){
+//                 map[d]=[];
+//             }
+
+//             map[d].push(mat[i][j]);
+            
+//         }
+//     }
+
+//     for(let key in map){
+//         map[key].sort((a, b) => {return a-b })
+//     }
+
+//     for(let i=0; i < mat.length; i++){
+//         for(let j=0; j < mat[0].length; j++){
+//             let d = i-j;
+//             mat[i][j] = map[d].shift();
+//         }
+//     }
+//     return mat;
