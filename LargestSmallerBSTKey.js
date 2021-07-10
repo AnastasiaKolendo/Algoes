36fdb956f719e24d7f8ef4f80711c67254e65099
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-07-09 22:09:23 -0400

    Add time and space complexity

--- LargestSmallerBSTKey.js
+++ LargestSmallerBSTKey.js
@@ -92,3 +92,23 @@ function Node(key) {
   var result = bst.findLargestSmallerKey(17);
   
   console.log("Largest smaller number is " + result);
+
+//   function findLargestSmallerKey(rootNode, num):
+//     result = -1
+
+//     while (rootNode != null):
+//         if (rootNode.key < num):
+//             result = rootNode.key
+//             rootNode = rootNode.right
+//         else:
+//             rootNode = rootNode.left
+
+//     return result
+
+
+//Time Complexity: we scan the tree once from the root to the the leaves and do a constant number of actions for each node. 
+//if the tree is balanced the complexity is O(log(N)). 
+//Otherwise, it could be up to O(N).
+
+//Space Complexity: throughout the entire algorithm we used only a constant amount of space, 
+//hence the space complexity is O(1).
