9f9f1e891ea9c88a4b120822967392c7d0c5a9a9
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-07-16 10:23:04 -0700

    Quick Sort

--- QuickSort.js
+++ QuickSort.js
@@ -1,21 +1,45 @@
-function partition(arr, start, end){
-    // Taking the last element as the pivot
-    const pivotValue = arr[end];
-    let pivotIndex = start; 
-    for (let i = start; i < end; i++) {
-        if (arr[i] < pivotValue) {
-        // Swapping elements
-        [arr[i], arr[pivotIndex]] = [arr[pivotIndex], arr[i]];
-        // Moving to next element
-        pivotIndex++;
+function quickSort(arr, left, right){
+    let pivot = partion(arr, left, right);
+
+    console.log(pivot, arr, 'here')
+
+    if(left < pivot - 1){
+        //sort leftHalf
+        quickSort(arr, left, pivot - 1)
+    }
+
+    if(pivot < right){
+        //sort rightHalf
+        quickSort(arr, pivot, right)
+    }
+}
+
+function partion(arr, left, right){
+    let mid = arr[left + Math.floor((right - left) / 2)];
+
+    while(left <= right){
+        while(arr[left] < mid){
+            left++;
+        }
+    
+        while(arr[right] > mid){
+            right--;
+        }
+    
+        if(left <= right){
+            let temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
         }
     }
     
-    // Putting the pivot value in the middle
-    [arr[pivotIndex], arr[end]] = [arr[end], arr[pivotIndex]] 
-    return pivotIndex;
-};
+    return left;
+}
 
 
-array = [7, -2, 4, 1, 6, 5, 0, -4, 2]
-console.log(partition(array, 0, array.length - 1))
+
+let array = [7, -2, 4, 1, 6, 5, 0, -4, 2]
+quickSort(array, 0, array.length - 1)
+console.log(array)
